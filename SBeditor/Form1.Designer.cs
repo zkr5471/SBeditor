51604c10b70ec2de28b6199cd84d9c064c17fd24
MrzkR <mrzkr2ds@gmail.com>
2021-02-28 01:54:10 +0900

    Initial commit

--- SBeditor/Form1.Designer.cs
+++ SBeditor/Form1.Designer.cs
@@ -29,13 +29,84 @@ namespace SBeditor
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+			this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(800, 500);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.BackColor = System.Drawing.Color.White;
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.openToolStripMenuItem.Text = "Open";
+			this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.Filter = "テキストファイル|*.txt|すべてのファイル|*.*";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(800, 524);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Shown += new System.EventHandler(this.Form1_Shown);
+			this.Resize += new System.EventHandler(this.Form1_Resize);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
 	}
 }
 
--- SBeditor/Form1.cs
+++ SBeditor/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,67 @@ namespace SBeditor
 {
 	public partial class Form1 : Form
 	{
+		List<StringBuilder> _data;
+		Bitmap _bitmap;
+
+		readonly Size MaxBitmapSize = new Size(1000, 600);
+
 		public Form1()
 		{
 			InitializeComponent();
+
+			_data = new List<StringBuilder>();
+			_bitmap = new Bitmap(MaxBitmapSize.Width, MaxBitmapSize.Height);
+
 		}
+
+		private void Form1_Shown(object sender, EventArgs e)
+		{
+			Render();
+		}
+
+		private void Form1_Resize(object sender, EventArgs e)
+		{
+
+		}
+
+		private void openToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				Openfile(openFileDialog1.FileName);
+				Render();
+			}
+		}
+
+		private void Render()
+		{
+			Graphics gra = Graphics.FromImage(_bitmap);
+			Font font = new Font("MeiryoKe_Console", 10);
+
+
+
+			pictureBox1.Image = _bitmap;
+
+			font.Dispose();
+			gra.Dispose();
+		}
+
+		private void Openfile(string path)
+		{
+			_data.Clear();
+
+			using (var file = new StreamReader(path))
+			{
+				string line;
+				while ((line = file.ReadLine()) != null)
+				{
+					_data.Add(new StringBuilder(line));
+				}
+			}
+		}
+
+
+
 	}
 }
