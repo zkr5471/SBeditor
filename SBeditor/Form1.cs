2024acf8ce607cd73b624ae75289e33085881ae1
MrzkR <mrzkr2ds@gmail.com>
2021-02-28 02:41:41 +0900

    fix functions in CursorPoint

--- SBeditor/Form1.cs
+++ SBeditor/Form1.cs
@@ -23,6 +23,22 @@ namespace SBeditor
 		{
 			public int X, Y;
 
+			private List<StringBuilder> source
+			{
+				get
+				{
+					return Form1.Instance.SourceData;
+				}
+			}
+
+			private StringBuilder cur_line
+			{
+				get
+				{
+					return source[Y];
+				}
+			}
+
 			public CursorPoint(int x, int y)
 			{
 				X = x;
@@ -32,10 +48,17 @@ namespace SBeditor
 			public void Next()
 			{
 				X++;
-				if (X > Form1.Instance.SourceData[Y].Length)
+				if (X > cur_line.Length)
 				{
-					X = 0;
-					Y++;
+					if (Y < source.Count() - 1)
+					{
+						X = 0;
+						Y++;
+					}
+					else
+					{
+						X--;
+					}
 				}
 			}
 
@@ -47,13 +70,42 @@ namespace SBeditor
 					if (Y > 0)
 					{
 						Y--;
-						X = Form1.Instance.SourceData[Y].Length;
+						X = cur_line.Length;
 					}
 					else
 					{
 						X = Y = 0;
 					}
 				}
+			}
+
+			public void Up()
+			{
+				if (Y == 0)
+				{
+					X = 0;
+					return;
+				}
+				else
+				{
+					Y--;
+					int len = cur_line.Length;
+					if (X > len) X = len;
+				}
+			}
+
+			public void Down()
+			{
+				Y++;
+
+				if (Y >= source.Count())
+				{
+					Y--;
+					X = cur_line.Length;
+					return;
+				}
+
+
 			}
 		}
 
@@ -199,12 +251,39 @@ namespace SBeditor
 
 		private void Form1_KeyDown(object sender, KeyEventArgs e)
 		{
-			
+			switch (e.KeyCode)
+			{
+				case Keys.Up:
+				{
+					CursorPos.Up();
+					break;
+				}
+
+				case Keys.Down:
+				{
+					CursorPos.Down();
+					break;
+				}
+
+				case Keys.Left:
+				{
+					CursorPos.Back();
+					break;
+				}
+
+				case Keys.Right:
+				{
+					CursorPos.Next();
+					break;
+				}
+			}
+
+		//	MessageBox.Show($"{CursorPos.X}, {CursorPos.Y}");
 		}
 
 		private void Form1_KeyPress(object sender, KeyPressEventArgs e)
 		{
-			if( CheckKeyChar(e.KeyChar))
+			if (CheckKeyChar(e.KeyChar))
 			{
 				InsertString(CursorPos, e.KeyChar.ToString());
 
